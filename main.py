8731d0810f90add3e5a5f8d7502f5e7eaa3d41b3
kolauzinho <167236590+kolauzinho@users.noreply.github.com>
2024-06-04 08:16:08 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def greet(name):
+    return "Tudo tem o seu tempo determinado, e há tempo para todo propósito debaixo do céu: , " + name + "!"
+
+if __name__ == "__main__":
+    print(greet("Nicollas"))
